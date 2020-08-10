28bc04a621067404c128ab5024e604911186ca02
GerardoQ <gerardoquispe65@gmail.com>
2020-08-09 21:49:01 -0500

    Updates

---
+++ precache-manifest.877c1495828da40120492d1bda2e4ff3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a914ee7976f396f716261689c50e16d",
+    "url": "/MiPortafolioGQ/index.html"
+  },
+  {
+    "revision": "a0e2dc9a29f68ace88b9",
+    "url": "/MiPortafolioGQ/static/css/main.372c1c25.chunk.css"
+  },
+  {
+    "revision": "7fe9460c5390feeeb6da",
+    "url": "/MiPortafolioGQ/static/js/2.a83c6dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MiPortafolioGQ/static/js/2.a83c6dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e2dc9a29f68ace88b9",
+    "url": "/MiPortafolioGQ/static/js/main.0feb6b37.chunk.js"
+  },
+  {
+    "revision": "f65958be24c27b9bad0b",
+    "url": "/MiPortafolioGQ/static/js/runtime-main.a4c8aacd.js"
+  },
+  {
+    "revision": "c678df99a81c59dadf39f4fdc9b1ed65",
+    "url": "/MiPortafolioGQ/static/media/ecommerce.c678df99.PNG"
+  },
+  {
+    "revision": "1fb4ab07de342b5ca1211453c74a9650",
+    "url": "/MiPortafolioGQ/static/media/scrumcerti.1fb4ab07.png"
+  },
+  {
+    "revision": "93234989cc84cc9319cdc4f5fcd2d204",
+    "url": "/MiPortafolioGQ/static/media/valtienda.93234989.PNG"
+  },
+  {
+    "revision": "27321e0ff58f947d6fe9e3a6d24e5c41",
+    "url": "/MiPortafolioGQ/static/media/venciapp.27321e0f.PNG"
+  }
+]);
